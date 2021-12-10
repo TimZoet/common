caa129b3cef62eea00a2729bb1ad96e248088c43
Tim Zoet <ts.zoet@gmail.com>
2021-12-10 19:50:53 +0100

    Made module installable and packageable with conan

---
+++ conanfile.py
@@ -0,0 +1,44 @@
+from conans import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake
+from conan.tools.layout import cmake_layout
+
+class CommonConan(ConanFile):
+    name = "common"
+    version = "0.1.0"
+
+    # Optional metadata
+    license = "<Put the package license here>"
+    author = "Tim Zoet"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Hello here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"fPIC": [True, False]}
+    default_options = {"fPIC": True}
+
+    exports_sources = "CMakeLists.txt", "readme.md", "cmake/*", "external/*", "modules/*", "tests/*"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["common"]
