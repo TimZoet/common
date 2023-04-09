e9d66f54b6eeeeff09f6d7ed1b756bd0dd768486
TimZoet <ts.zoet@gmail.com>
2023-04-09 16:16:02 +0200

    Bumped version and updated version history.

--- commonVersionString.cmake
+++ commonVersionString.cmake
@@ -1,4 +1,4 @@
 set(COMMON_VERSION_MAJOR 1)
-set(COMMON_VERSION_MINOR 0)
+set(COMMON_VERSION_MINOR 1)
 set(COMMON_VERSION_PATCH 0)
-set(COMMON_VERSION 1.0.0) # Leave version string here explicitly for parsing.
+set(COMMON_VERSION 1.1.0) # Leave version string here explicitly for parsing.
